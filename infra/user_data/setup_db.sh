7b2ee71aa0fd16060ba50e5b60625f5bb7e00ced
dvrs <dvrsdev@gmail.com>
2026-01-10 22:17:20 -0400

    Ajustes no pipeline(fix): configurado self-hosted agent

--- infra/user_data/setup_db.sh
+++ infra/user_data/setup_db.sh
@@ -8,12 +8,12 @@ curl -fsSL https://get.docker.com -o get-docker.sh
 sudo sh get-docker.sh
 sudo usermod -aG docker ubuntu
 
-# 2. setup das pastas
+#  setup das pastas
 sudo mkdir -p /opt/prova-db
 sudo chown -R ubuntu:ubuntu /opt/prova-db
 cd /opt/prova-db
 
-# 3. sql para criar tabela e inserir dados
+# .sql para criar tabela e inserir dados
 cat <<EOF > init.sql
 CREATE TABLE usuarios (id SERIAL PRIMARY KEY, nome VARCHAR(50), cargo VARCHAR(50), situacao VARCHAR(50));
 INSERT INTO usuarios (nome, cargo,situacao) VALUES ('Douglas', 'DevOps/NOC1','Aprovado');
